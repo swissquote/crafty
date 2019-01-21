e07071bed4815211b6339d7992c1a722d7132c4b
Stéphane Goetz <onigoetz@onigoetz.ch>
2019-01-21 23:20:06 +0100

    Prepare for webpack 5

--- packages/integration/__tests__/crafty-preset-typescript-webpack.js
+++ packages/integration/__tests__/crafty-preset-typescript-webpack.js
@@ -90,14 +90,14 @@ it("Compiles TypeScript", () => {
 
   expect(fs.existsSync("dist/js/myBundle.min.js")).toBeTruthy();
   expect(fs.existsSync("dist/js/myBundle.min.js.map")).toBeTruthy();
-  expect(fs.existsSync("dist/js/1.myBundle.min.js")).toBeTruthy();
-  expect(fs.existsSync("dist/js/1.myBundle.min.js.map")).toBeTruthy();
+  expect(fs.existsSync("dist/js/944.myBundle.min.js")).toBeTruthy();
+  expect(fs.existsSync("dist/js/944.myBundle.min.js.map")).toBeTruthy();
 
   expect(
     fs.readFileSync("dist/js/myBundle.min.js").toString("utf8")
   ).toMatchSnapshot();
   expect(
-    fs.readFileSync("dist/js/1.myBundle.min.js").toString("utf8")
+    fs.readFileSync("dist/js/944.myBundle.min.js").toString("utf8")
   ).toMatchSnapshot();
 });
 
