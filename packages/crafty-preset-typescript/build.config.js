5559606c029bd6b2d6e2b02c627026bed9a79e39
Stéphane Goetz <stephane.goetz@swissquote.ch>
2021-10-12 10:59:09 +0200

    Make sure the right version of webpack is loaded for ts-loader

--- packages/crafty-preset-typescript/build.config.js
+++ packages/crafty-preset-typescript/build.config.js
@@ -3,11 +3,14 @@ module.exports = [
   {
     name: "typescript-packages",
     externals: {
+      // Dependencies of this package
       "@babel/core": "@babel/core",
       "@babel/code-frame": "@babel/code-frame",
       "@babel/helper-module-imports": "@babel/helper-module-imports",
       typescript: "typescript",
-      webpack: "webpack",
+
+      // Provided by other Crafty packages
+      webpack: "@swissquote/crafty-runner-webpack/packages/webpack",
     }
   }
 ];
