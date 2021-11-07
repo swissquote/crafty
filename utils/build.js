8474fcc9acfbb92bff083362ca0e1695c57a8d72
Stéphane Goetz <onigoetz@onigoetz.ch>
2021-11-07 21:41:50 +0100

    Add ignore files

--- utils/build.js
+++ utils/build.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 
+const rimraf = require("rimraf");
 const compile = require("./compile.js");
 const configuration = require(process.cwd() + "/build.config.js");
 
 async function main() {
+
+  // Start with a cleanup
+  rimraf.sync(process.cwd() + '/dist/compiled');
+
   for (const bundle of configuration) {
     const { name, ...options } = bundle;
     await compile(`./src/${name}.js`, `dist/compiled/${name}.js`, options);
