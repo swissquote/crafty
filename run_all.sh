bb871f06c7edeba4da0d74a77ba91de7860404b7
Stéphane Goetz <onigoetz@onigoetz.ch>
2021-12-08 21:54:30 +0100

    Use bash as interpreter for run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 GREEN='\033[0;32m'
 NC='\033[0m'
