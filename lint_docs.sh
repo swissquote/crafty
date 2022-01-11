55eb5a10dad52886b92ff2e5772723a7916d0f7b
Stéphane Goetz <onigoetz@onigoetz.ch>
2022-01-11 23:50:58 +0100

    Fix linting commands

--- lint_docs.sh
+++ lint_docs.sh
@@ -7,11 +7,11 @@ set -e
 
 echo -e "${GREEN}Autoformatting Docs${NC}"
 echo -e "${GREEN}===================${NC}"
-yarn prettier --write docs/*.md docs/**/*.md docs/**/**/*.md *.md
+node_modules/.bin/prettier --write docs/*.md docs/**/*.md docs/**/**/*.md *.md
 
 echo
 echo -e "${GREEN}Write Good${NC}"
 echo -e "${GREEN}==========${NC}"
-yarn write-good --no-passive --no-illusion docs/*.md docs/**/*.md docs/**/**/*.md *.md
+node_modules/.bin/write-good --no-passive --no-illusion docs/*.md docs/**/*.md docs/**/**/*.md *.md
 
 echo -e "${GREEN}All good${NC}"
