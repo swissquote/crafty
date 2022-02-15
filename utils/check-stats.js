0846b77156b2764b85e883e68468f2ffc9752403
Stéphane Goetz <onigoetz@onigoetz.ch>
2022-02-15 21:29:19 +0100

    Check build sanity

--- utils/check-stats.js
+++ utils/check-stats.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 const path = require("path");
 const file = process.argv[2];
+const loadFile = path.join(process.cwd(), file);
 
-const data = require(path.join(process.cwd(), file));
+const data = require(loadFile);
 
 const errors = [];
 
@@ -12,22 +13,57 @@ function recordError(error) {
 
 // All packages must be found
 data.modules
-  .filter(m => m.name.indexOf("/ncc/@@notfound") > -1)
-  .map(m => `Module "${m.name.split("?")[1]}" requested by "${m.issuerName}" was not found.`)
+  .filter((m) => m.name.indexOf("/ncc/@@notfound") > -1)
+  .map(
+    (m) =>
+      `Module "${m.name.split("?")[1]}" requested by "${
+        m.issuerName
+      }" was not found.`
+  )
   .map(recordError);
 
 // Packages provided by another module should stay external
 data.modules
   .filter((m) => m.name.indexOf("/packages/") > -1)
   .filter((m) => m.name.indexOf("external ") !== 0)
-  .map((m) => `Module "${m.name}" requested by "${m.issuerName}" should be external.`)
+  .map(
+    (m) =>
+      `Module "${m.name}" requested by "${m.issuerName}" should be external.`
+  )
   .map(recordError);
 
 // All readable-stream packages must be external
 data.modules
-  .filter((m) => m.name.indexOf("readable-stream") > -1 || m.name.indexOf("stylelint/") > -1)
   .filter((m) => m.name.indexOf("external ") !== 0)
-  .map((m) => `Module "${m.name}" requested by "${m.issuerName}" should be external.`)
+  .filter((m) => {
+
+    // Never accept those packages
+    if (
+      m.name.indexOf("node_modules/readable-stream/") > -1 ||
+      m.name.indexOf("node_modules/typescript/") > -1
+    ) {
+      return true;
+    }
+
+    // Only accept stylelint in some files
+    if (
+      m.name.indexOf("node_modules/stylelint/") > -1 &&
+      !loadFile.includes(
+        "stylelint-config-swissquote/dist/compiled/stylelint_-utils-packages-stats.json"
+      ) &&
+      !loadFile.includes(
+        "crafty-preset-postcss/dist/compiled/packages-gulp-stats.json"
+      )
+    ) {
+      return true;
+    }
+
+    return false;
+  })
+  .map(
+    (m) =>
+      `Module "${m.name}" requested by "${m.issuerName}" should be external.`
+  )
   .map(recordError);
 
 if (errors.length > 0) {
@@ -38,4 +74,5 @@ if (errors.length > 0) {
   });
 
   console.log();
+  process.exit(1);
 }
