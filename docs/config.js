428bcddc24cff0504bf7befc5861c1d76b6cca82
Jonathan Link <jonathan.link@swissquote.ch>
2024-04-11 14:55:24 +0200

    Remove google analytics from the config file

---
+++ docs/config.js
@@ -0,0 +1,16 @@
+{
+    "title": "Crafty",
+    "tagline": "The task runner that binds them all",
+    "author": "Swissquote Bank SA",
+    "html": {
+      "theme": "swissquote",
+      "repo": "swissquote/crafty",
+      "toggle_code": false, 
+      "links": {
+        "Technical blog": "https://medium.com/swissquote-engineering",
+        "Swissquote": "https://swissquote.com/",
+        "GitHub": "http://github.com/swissquote/crafty"
+      },
+      "powered_by": "© 2018 Swissquote Bank SA. Powered by Daux.io."
+    }
+  }
