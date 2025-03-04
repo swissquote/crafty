757d8d023b9f79e8650b1eab5052a9e06799cedf
Stéphane Goetz <stephane.goetz@onigoetz.ch>
2025-03-04 21:58:43 +0100

    Add tests for more recent versions of TypeScript

---
+++ packages/integration/fixtures/crafty-preset-typescript/test-ts-syntax_prettier3/js/TS_5_6.ts
@@ -0,0 +1,5 @@
+// https://devblogs.microsoft.com/typescript/announcing-typescript-5-6/
+
+// https://devblogs.microsoft.com/typescript/announcing-typescript-5-6/#support-for-arbitrary-module-identifiers
+const banana = "🍌";
+export { banana as "🍌" };
