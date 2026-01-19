79ee6b56f21a4c865b0fb5298e30a383532bb5a8
Paulo Miranda <pauloabmiranda@gmail.com>
2026-01-19 12:40:03 +0000

    Fix eslint generated template (#2941)
    * Fix eslint generated template
    
    * Update packages/crafty-preset-eslint/src/templates.js
    
    Co-authored-by: Stéphane Goetz <onigoetz@onigoetz.ch>
    
    * Update templates.js
    
    ---------
    
    Co-authored-by: Stéphane Goetz <onigoetz@onigoetz.ch>

--- packages/crafty-preset-eslint/src/templates.js
+++ packages/crafty-preset-eslint/src/templates.js
@@ -31,18 +31,14 @@ function ideConfiguration(crafty) {
 // This file helps IDEs autoconfigure themselves
 // any change here will only be used by your IDE, not by Crafty
 
-/* global process */
+import { initialize } from "${CRAFTY_PATH}";
+import { toESLintConfig } from "${ESLINT_PRESET_PATH}";
 
-module.exports = (async () => {
-  const { initialize } = await import("@swissquote/crafty");
-  const { toESLintConfig } = await import("@swissquote/crafty-preset-eslint");
-  
-  const crafty = await initialize(process.argv, ${cliConfig});
-  
-  const config = await toESLintConfig(crafty, {}, "ide");
-  
-  return config;
-})();
+const crafty = await initialize(process.argv, ${cliConfig});
+
+const config = await toESLintConfig(crafty, {}, "ide");
+
+export default config;
 `;
 }
 
